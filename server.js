04c8f954381d3a8454b07c30911ce81b24d56618
Rachel Elam <relam24@gmail.com>
2018-05-03 14:32:54 -0400

    got everything working except edit

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-app.use(express.urlencooded({ extended: false }));
+// body parser needs to be above controllers
+// method override
+// static files (css/js)
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.json());
 
+// controllers
+// tournaments
 const opryController = require('./controllers/opry.js');
 app.use('/', opryController);
 
-const mongoURI = process.end.MONGOD_URI || 'mongodb://localhost:27017/opry';
+// connections
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/opry';
 mongoose.connect(mongoURI);
 mongoose.connection.once('open', () => {
-	console.log('connected to mongod');
+	console.log('connected to mongo');
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
